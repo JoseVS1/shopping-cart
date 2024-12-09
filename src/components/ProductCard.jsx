9045b1450657e05ee8c543852e461fe11ea1e854
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2024-12-09 17:22:46 -0600

    Populate shop page with product cards

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const ProductCard = ({product}) => {
+  return (
+    <div>
+        <h2>{product.title}</h2>
+        <img src={product.image} alt={product.title} />
+        <p>{product.description}</p>
+        <span>${product.price}</span>
+        <div>
+            <button>-</button>
+            <span>0</span>
+            <button>+</button>
+        </div>
+        
+        <button>Add</button>
+    </div>
+  )
+}
--- src/components/pages/ShopPage.jsx
+++ src/components/pages/ShopPage.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ProductCard } from '../ProductCard';
 
 export const ShopPage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getAllProducts = async () => {
+            const response = await fetch("https://fakestoreapi.com/products");
+            const data = await response.json();
+
+            setProducts(data);
+        }
+
+        getAllProducts()
+    }, [])
   return (
-    <div>ShopPage</div>
+    <>
+        {products.map(p => {
+            return <ProductCard key={p.id} product={p} />
+        })}
+    </>
   )
 }
