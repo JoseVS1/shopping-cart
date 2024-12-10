dcd8eb69269e2991a4d2a82d4ccf71b1cd182dad
JoseVS1 <105169283+JoseVS1@users.noreply.github.com>
2024-12-10 13:22:10 -0600

    Style navbar

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 export const HomePage = () => {
   return (
-    <>
+    <div className='home-container'>
         <h1>NovaNest</h1>
 
         <p>Discover the Next Generation of Home & Lifestyle Products</p>
 
         <Link to="shop">Shop Now</Link>
-    </>
+    </div>
   )
 }
